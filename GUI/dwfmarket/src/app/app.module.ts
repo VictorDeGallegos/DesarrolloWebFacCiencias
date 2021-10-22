a141de7e1b8a7aa2c56cea9beec79d8be2a89b57
Victor Gallegos <316160456@ciencias.unam.mx>
2021-10-21 21:30:00 -0500

    Implementamos Componente TS Exchange Rate

--- GUI/dwfmarket/src/app/app.module.ts
+++ GUI/dwfmarket/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http'
 import { LayoutModule } from './modules/layout/layout.module';
 import { ExchangeRateModule} from './modules/exchange-rate/exchange-rate.module';
 
@@ -13,6 +14,7 @@ import { ExchangeRateModule} from './modules/exchange-rate/exchange-rate.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     LayoutModule,
     ExchangeRateModule
   ],
