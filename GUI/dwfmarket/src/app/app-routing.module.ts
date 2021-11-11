1477fa17d186a2a41984f341a2400ceb95461f75
Victor Gallegos <316160456@ciencias.unam.mx>
2021-11-11 17:02:29 -0600

    CRUD category

--- GUI/dwfmarket/src/app/app-routing.module.ts
+++ GUI/dwfmarket/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExchangeRateComponent } from '../app/modules/exchange-rate/component/exchange-rate/exchange-rate.component';
 import { RegionComponent } from '../app/modules/customer/component/region/region.component';
-import { HomeComponent } from '../app/modules/home/component/home/home.component';
+import { CategoryComponent } from '../app/modules/product/component/category/category.component';
 
 const routes: Routes = [
   {path: 'exchange-rate', component: ExchangeRateComponent},
   {path: 'region', component: RegionComponent},
-  {path: '', component: HomeComponent}
+  {path: 'category', component: CategoryComponent}
 ];
 
 @NgModule({
